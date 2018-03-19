786356b2ce861783a072cd038a1021ca9838f876
廖师兄 <null@163.com>
2018-03-19 09:44:34 +0800

    feign-hystrix

--- client/src/main/java/com/imooc/product/client/ProductClient.java
+++ client/src/main/java/com/imooc/product/client/ProductClient.java
@@ -3,6 +3,7 @@ package com.imooc.product.client;
 import com.imooc.product.common.DecreaseStockInput;
 import com.imooc.product.common.ProductInfoOutput;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,7 +13,7 @@ import java.util.List;
  * Created by 廖师兄
  * 2017-12-10 21:04
  */
-@FeignClient(name = "product")
+@FeignClient(name = "product", fallback = ProductClient.ProductClientFallback.class)
 public interface ProductClient {
 
     @PostMapping("/product/listForOrder")
@@ -20,4 +21,18 @@ public interface ProductClient {
 
     @PostMapping("/product/decreaseStock")
     void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+
+    @Component
+    static class ProductClientFallback implements ProductClient {
+
+        @Override
+        public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
+
+        }
+    }
 }
