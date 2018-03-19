f2ca2317b0e160f713b37591902b9fa16b9c013e
廖师兄 <null@163.com>
2018-03-19 09:43:56 +0800

    restTemplate-hystrix

--- server/src/main/java/com/imooc/product/controller/ProductController.java
+++ server/src/main/java/com/imooc/product/controller/ProductController.java
@@ -76,11 +76,17 @@ public class ProductController {
 
     /**
      * 获取商品列表(给订单服务用的)
+     *
      * @param productIdList
      * @return
      */
     @PostMapping("/listForOrder")
     public List<ProductInfoOutput> listForOrder(@RequestBody List<String> productIdList) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return productService.findList(productIdList);
     }
 
